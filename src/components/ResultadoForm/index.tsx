22fe06188d616ee8d6901a1556bea483054904e1
araimundo362 <araimundo362@gmail.com>
2023-08-15 19:47:35 -0300

    fix: carga resultado

--- src/components/ResultadoForm/index.tsx
+++ src/components/ResultadoForm/index.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonCol, IonIcon, IonInput, IonItem, IonLabel, IonRow } from "@ionic/react";
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCol, IonIcon, IonInput, IonItem, IonLabel, IonList, IonRow } from "@ionic/react";
 import React, { useContext, useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ResultadosContext } from "../../context/ResultadosContext";
@@ -42,6 +42,10 @@ const ResultadoForm: React.FC = () => {
     }
 
     const handlePrimerSetInput = (value: any) => {
+        
+        if (value && value.length === 1 && primerSetInputRef.current) {
+            primerSetInputRef.current.value = value + "-";
+        }
         if (value?.length === 3) {
             setValue("primerSet", value);
             segundoSetInputRef.current?.setFocus();
@@ -49,6 +53,9 @@ const ResultadoForm: React.FC = () => {
     }
 
     const handleSegundoSetInput = (value: string | null | undefined) => {
+        if (value && value.length === 1 && segundoSetInputRef.current) {
+            segundoSetInputRef.current.value = value + "-";
+        }
         if (value?.length === 3) {
             setValue("segundoSet", value);
             tercerSetInputRef.current?.setFocus();
@@ -56,18 +63,44 @@ const ResultadoForm: React.FC = () => {
     }
 
     const handleTercerSetInput = (value: string | null | undefined) => {
+        if (value && value.length === 1 && tercerSetInputRef.current) {
+            tercerSetInputRef.current.value = value + "-";
+        }
         if (value?.length === 3) {
             tercerSetInputRef.current?.setBlur();
             setValue("tercerSet", value);
         }
     }
 
+    const keyDownPressPrimerSet = (ev: any) => {
+        if (ev.key === "Backspace" && primerSetInputRef.current) {
+            primerSetInputRef.current.value = ""
+        }
+    }
+
+    const keyDownPressSegundoSet = (ev: any) => {
+        if (ev.key === "Backspace" && segundoSetInputRef.current) {
+            segundoSetInputRef.current.value = ""
+        }
+    }
+
+    const keyDownPressTercerSet = (ev: any) => {
+        if (ev.key === "Backspace" && tercerSetInputRef.current) {
+            tercerSetInputRef.current.value = ""
+        }
+    }
+
     return <React.Fragment>
-        <IonRow>
-            <IonCol size="10" offset="1">
-                <h3 className="cargar-resultado__label">Decinos como salio el partido!</h3>
-            </IonCol>
-        </IonRow>
+        <IonCard color="light">
+            <IonCardHeader>
+                    <IonCardTitle className="card-title"><span>Importante!</span></IonCardTitle>
+                    <IonCardSubtitle className="card-subtitle">Ejemplo: 5-7 6-4 7-6 // 6-4 6-0</IonCardSubtitle>
+            </IonCardHeader>
+
+            <IonCardContent>
+                <IonLabel className="label-aviso">Ambos jugadores deberan cargar el mismo resultado, y marcar el boton correspondiente!</IonLabel>
+            </IonCardContent>
+        </IonCard>
         <form onSubmit={handleSubmit(onSubmit)}>
         <IonRow>
             <IonCol size="10" offset="1">
@@ -78,19 +111,19 @@ const ResultadoForm: React.FC = () => {
             <IonCol size="4">
                 <IonItem fill="solid" className="resultado-form__score-input">
                     <IonLabel position="floating">1er set*</IonLabel>
-                    <IonInput {...register("primerSet", {required: true})} ref={primerSetInputRef} max={2} onIonChange={(ev) => handlePrimerSetInput(ev.detail.value)} />
+                    <IonInput onKeyDown={ev => keyDownPressPrimerSet(ev)} {...register("primerSet", {required: true})} ref={primerSetInputRef} max={2} onIonChange={(ev) => handlePrimerSetInput(ev.detail.value)} />
                 </IonItem>
             </IonCol>
             <IonCol size="4">
             <IonItem fill="solid" className="resultado-form__score-input">
                     <IonLabel position="floating">2do set*</IonLabel>
-                    <IonInput {...register("segundoSet", {required: true})} max={2} onIonChange={(ev) => handleSegundoSetInput(ev.detail.value)}  ref={segundoSetInputRef}/>
+                    <IonInput onKeyDown={ev => keyDownPressSegundoSet(ev)} {...register("segundoSet", {required: true})} max={2} onIonChange={(ev) => handleSegundoSetInput(ev.detail.value)}  ref={segundoSetInputRef}/>
                 </IonItem>
             </IonCol>
             <IonCol size="4">
             <IonItem fill="solid" className="resultado-form__score-input">
                     <IonLabel position="floating">3er set*</IonLabel>
-                    <IonInput {...register("tercerSet")} max={2}  ref={tercerSetInputRef} onIonChange={(ev) => handleTercerSetInput(ev.detail.value)}/>
+                    <IonInput onKeyDown={ev => keyDownPressTercerSet(ev)} {...register("tercerSet")} max={2}  ref={tercerSetInputRef} onIonChange={(ev) => handleTercerSetInput(ev.detail.value)}/>
                 </IonItem>
             </IonCol>
         </IonRow>
