146da0daa2e189c05b11c7529e4ae87eea653697
araimundo362 <araimundo362@gmail.com>
2023-08-30 16:21:52 -0300

    feature: resultados banner for admin

--- src/context/GlobalContext.tsx
+++ src/context/GlobalContext.tsx
@@ -1,25 +1,32 @@
 import React, { useState, ReactNode, useEffect } from 'react';
 import { Categoria } from '../interfaces/categoria';
 import { getCategorias } from '../axios/categorias';
+import { ResultadosResponse } from '../interfaces/resultado';
+import { obtenerResultados } from '../axios/resultados';
 
 type GlobalContextProps = {
     children: ReactNode,
 };
 
 export const GlobalContext = React.createContext<{
-    categorias: Categoria[]
+    categorias: Categoria[],
+    resultados: ResultadosResponse[]
 }>({} as any);
 
 export const GlobalContextProvider = ({ children }: GlobalContextProps) => {
 
     const [categorias, setCategorias] = useState<Categoria[]>([]);
 
+    const [resultados, setResultados] = useState<ResultadosResponse[]>([]);
+
     useEffect(() => {
         getCategorias().then((data: Categoria[]) => {
             setCategorias(data);
         });
-    }, [])
-    return <GlobalContext.Provider value={{ categorias }}>
+        obtenerResultados().then((data: ResultadosResponse[]) => setResultados(data));
+    }, []);
+    
+    return <GlobalContext.Provider value={{ categorias, resultados }}>
         {children}
     </GlobalContext.Provider>
 }
